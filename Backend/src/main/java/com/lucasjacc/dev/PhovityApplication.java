c2189fa9c8a893657664ea7ef6109510b3211586
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2026-02-03 11:22:45 -0300

    first commit

---
+++ Backend/src/main/java/com/lucasjacc/dev/PhovityApplication.java
@@ -0,0 +1,13 @@
+package com.lucasjacc.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhovityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhovityApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/lucasjacc/dev/PhovityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasjacc.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhovityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
