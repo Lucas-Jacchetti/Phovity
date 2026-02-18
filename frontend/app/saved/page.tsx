3ccb8b4ef4d6c4a928090dd5885d732cbd0bc01e
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2026-02-17 22:30:12 -0300

    pagina salvos (vazia)

---
+++ frontend/app/saved/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Sidebar } from "../components/Sidebar"
+
+export default function Saved() {
+    return (
+        <div className="w-full">
+            <Sidebar/>
+            <div className="min-h-screen bg-white px-8 py-10">
+                <div className="flex flex-col">
+                    <div className="flex flex-row">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
